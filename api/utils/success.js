2f2af134b5f52913c55c2c4c562ae7df92647fc7
Asen Chochev <asen.qkiqt145@gmail.com>
2024-12-10 22:15:58 +0200

    Delete api directory

--- api/utils/success.js
+++
@@ -1,8 +0,0 @@
-export const createSuccess = (statusCode, successMessage, data) => {
-    const successObj = {
-        status: statusCode,
-        message: successMessage,
-        data: data
-    }
-    return successObj;
-}
